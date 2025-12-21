a380e5162920a8c601ea8699d60bfee343fcdcbd
BrunoMGCardoso <bruno.mg.cardoso.1991@gmail.com>
2025-12-21 03:51:22 +0000

    Versão 1.1

--- Search_engine_imdb_movies/App/app.py
+++ Search_engine_imdb_movies/App/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import busca
 
 lista_filmes = pd.read_csv('../base_dados/filmes_imdb.csv')
 
@@ -7,7 +8,7 @@ lista_filmes = pd.read_csv('../base_dados/filmes_imdb.csv')
 
 st.set_page_config(
     page_title="Pesquisa de Filmes",
-    page_icon="../multimedia/page_icon.ico",  # Pode ser emoji, caminho de arquivo ou URL
+    page_icon="../multimedia/page_icon.ico",
     layout="wide"
 )
 
@@ -21,14 +22,45 @@ with st.sidebar:
         st.write(lista_filmes['Movie Name'])
 
 # --- Página principal --- #
-   
-st.title('Search Engine de filmes do IMDB')
+  
+st.title(':streamlit: :blue[Filmes top 1000 do IMDB]', text_alignment = 'center', )
 
-st.header('')
+st.write('Este é um motor de busca onde podes procurar através de uma palavra-chave pelo filme que pretendes saber mais informações.')
 
-filme = st.text_input('Nome do Filme:', placeholder= 'Escreva aqui o nome do filme')
+col1, col2 = st.columns(2)
 
-pesquisar = st.button('Pesquisar')
+with col1:
+    
+    with st.container():
+        
+        filme = st.text_input('Nome do Filme:', placeholder= 'Escreva aqui o nome do filme', width = 500)
 
-if pesquisar:
-    st.subheader('Resultado da Pesquisa')
+        pesquisar = st.button('Pesquisar')
+ 
+        if pesquisar:
+            
+            st.session_state.lista.clear()
+            
+            if 'lista' not in st.session_state:
+                st.session_state.lista = []
+            
+            st.subheader('Resultado da Pesquisa:')
+            
+            with st.container(border= False, width= 500):
+                
+                if len(filme) >= 3:
+            
+                    resultado = busca.busca(filme)
+
+                    for nome, corr, index in resultado:
+                        st.write(f'✅ {nome} (Semelhança: {corr :.1f} %)')
+                        st.session_state.lista.append(nome)
+                    
+                else:
+                    st.error('Digite no mínimo 3 caracteres para a pesquisa')
+        
+with col2:
+    nome_filme = st.selectbox('Lista de filmes', st.session_state.lista, placeholder='Filme')
+    st.header(nome_filme) # Nome do Filme
+    st.subheader() # Ano do Filme
+    st.write() # Descrição do filme
--- Search_engine_imdb_movies/App/busca.py
+++ Search_engine_imdb_movies/App/busca.py
@@ -1,8 +1,12 @@
 from rapidfuzz import process
 import pandas as pd
 
-df = pd.read_csv('.base_dados/filmes_imdb.csv')
+df = pd.read_csv('../base_dados/filmes_imdb.csv')
 
 def busca(palavra_chave):
-    process.extract(query=palavra_chave, choices= df['Movie Name'])
+    resultado = process.extract(query=palavra_chave, choices= df['Movie Name'])
+    return resultado
+
+if __name__ == '__main__':
+    print(busca('God'))
     
